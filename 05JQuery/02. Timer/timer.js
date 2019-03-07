f82d4420f5aab3c3ef202365d60e36620c73319c
Milen Nikolov <milennikolov_bg@yahoo.com>
2019-03-08 00:01:18 +0200

    05jQuery

--- 05JQuery/02. Timer/timer.js
+++ 05JQuery/02. Timer/timer.js
@@ -1,3 +1,40 @@
 function timer() {
-   // TODO:
-}
+      let secondsSpan = $('#seconds');
+      let minutesSpan = $('#minutes');
+      let hoursSpan = $('#hours');
+      let isTicking = false;
+    
+      let timer;
+    
+      //events
+        $('#start-timer').on('click', function(){
+          if(!isTicking){
+            timer = setInterval(secondsUpdate, 1000);
+            isTicking = true;
+          }
+      });
+    
+      $('#stop-timer').on('click', function(){
+          if(isTicking){
+            clearInterval(timer);
+            isTicking = false;
+          }
+      });
+    
+      function secondsUpdate(){
+         secondsSpan.text(formatter(+secondsSpan.text() + 1));
+
+         if(+secondsSpan.text() === 60){
+            secondsSpan.text('00');
+            minutesSpan.text(formatter(+minutesSpan.text() + 1));
+         } else if(+minutesSpan.text() === 60){
+            minutesSpan.text('00');
+            hoursSpan.text(formatter(+hoursSpan.text() + 1));
+         }
+      }
+
+      function formatter(time){
+         return ("0" + time).slice(-2);
+      }
+    }
+    
--- 05JQuery/03. Form-Validation/form-validation.js
+++ 05JQuery/03. Form-Validation/form-validation.js
@@ -1,3 +1,92 @@
 function validate() {
-	// TODO:
+	
+	$('#submit').on('click', checkData);
+	
+	function checkData(event) {
+		event.preventDefault();
+		
+		let username = $('#username');
+		let email = $('#email');
+		let password = $('#password');
+		let confirmpassword = $('#confirm-password');
+		let isValid = true;
+
+		let usernamePattern = /^[a-zA-Z0-9]{3,20}$/;
+		if(!usernamePattern.test(username.val())){
+			username.css('border-color', 'red');
+			isValid = false;
+		} else {
+			username.css('border', 'none');
+		}
+
+		let emailPattern = /(.+)?@(.+)?\.(.+)?/g;
+		if(!emailPattern.test(email.val())){
+			email.css('border-color', 'red');
+			isValid = false;
+		} else {
+			email.css('border', 'none');
+		}
+
+		let passwordPattern = /\w{5,15}$/;
+		if(!passwordPattern.test(password.val())){
+			password.css('border-color', 'red');
+			isValid = false;
+		} else {
+			password.css('border', 'none');
+		}
+
+		if(!passwordPattern.test(confirmpassword.val()) || password.val() !== confirmpassword.val()){
+			confirmpassword.css('border-color', 'red');
+			password.css('border-color', 'red');
+			isValid = false;
+		}
+
+		let companyNumber = $('#companyNumber');
+
+		if($('#companyInfo').css('display') === 'block' &&
+		(companyNumber.val() < 1000 || companyNumber.val() > 9999)){
+			companyNumber.css('border-color', 'red');
+			isValid = false;
+		}
+
+		if(isValid){
+			$('#valid').css('display', 'block');
+		}
+	};
+
+	$('#company').on('click', showHideCompanyInfo);
+
+
+	function validateEmail() {
+		
+	}
+
+	// function validateConfirmPassword(){
+	// 	let password = $('#password').val();
+	// 	let confirmpassword = $('#confirm-password').val();
+	// 	if(confirmpassword !== password){
+	// 		confirmpassword.css('border-color', 'red');
+	// 	} else {
+	// 		confirmpassword.css('border', 'none');
+	// 	}
+	// }
+
+	function showHideCompanyInfo(){
+		let companyInfo = $('#companyInfo');
+
+		if(companyInfo.css('display') === 'none'){
+			companyInfo.css('display', 'block');
+		} else {
+			companyInfo.css('display', 'none');
+		}
+	}
+
+	// function validateCompany(){
+	// 	let isCompanyCheckbox = $('#company');
+	// 	if($('#company: checkbox:checked')){
+	// 		$('#companyInfo').css('style', '');
+	// 		validateCompanyNumber;
+	// 	}
+	// }
+	
 }
